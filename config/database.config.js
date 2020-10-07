48ca2794ee89a1823a550f227ec77d48fc55ccfd
Uday Pratap <udaypratap169@gmail.com>
2020-10-07 19:26:31 +0530

    mongo string changed to mongo_uri

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-   // url: process.env.MONGO_URI || 'mongodb://localhost:27017/api-test'
-   url:'mongodb+srv://uday:Uday%40mongo@cluster0.3dbzw.mongodb.net/api-test?retryWrites=true&w=majority'
+   url: process.env.MONGO_URI || 'mongodb://localhost:27017/api-test'
+
 }
